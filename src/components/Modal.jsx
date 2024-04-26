264be2b23cde1e7e714affb3102771d7d3207779
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2024-04-27 02:34:02 +0600

    responsive modal size

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -24,10 +24,10 @@ export default function Modal({ modalToggle, title, data, isOpen }) {
 
   return (
     <>
-      <div className="justify-center bg-[#061819d6] modal items-center flex overflow-x-hidden overflow-y-auto fixed  inset-0  z-50 outline-none focus:outline-none">
-        <div className=" absolute top-0 bottom-o flex left-0 right-0 items-center   py-4  w-auto  mx-auto max-w-3xl  ">
+      <div className="justify-center bg-[#061819d6]  modal items-center flex overflow-x-hidden overflow-y-auto fixed  inset-0  z-50 outline-none focus:outline-none">
+        <div className=" absolute top-0 bottom-o flex left-0 right-0 items-center   py-4 w-[calc(95%)]  sm:w-[calc(80%)]  mx-auto max-w-3xl   justify-center">
           {/* content  */}
-          <div ref={popupRef}>
+          <div ref={popupRef} className="w-full">
             <Form modalToggle={modalToggle} data={data} title={title} />
           </div>
         </div>
